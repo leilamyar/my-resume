4977068ffa28704aa3a3720b1ad681b8b5ea1026
leilamyar26 <leilamyar.dev@gmail.com>
2020-09-21 12:11:48 +0200

    feat(Jobs) : display jobs list title employer

--- my-app/src/Jobs.js
+++ my-app/src/Jobs.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import { JobsContainerStyled } from './JobsStyled'
+import { 
+  JobsContainerStyled, 
+  JobsList,
+  JobItem, JobTitle 
+} from './JobsStyled'
 const jobs = require('./data/jobs.json');
 
 const Jobs = () => {
   return (
-    <JobsContainerStyled>${JSON.stringify(jobs)}</JobsContainerStyled>
+    <JobsContainerStyled>
+      <JobsList>
+      {jobs.map(({ id, jobTitle, employer }) => (
+        <JobItem key={id}>
+          <JobTitle>{jobTitle}</JobTitle>
+          <div>{employer}</div>
+        </JobItem>
+      ))}
+      </JobsList>
+    </JobsContainerStyled>
   )
 }
 
